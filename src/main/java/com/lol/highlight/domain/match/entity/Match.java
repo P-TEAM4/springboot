17ad3a6b4730997701ed8fc2a3f67bf866b6df1e
Azin <setiguy@gachon.ac.kr>
2026-02-14 00:27:01 +0900

    dataddragon 오류 수정

--- src/main/java/com/lol/highlight/domain/match/entity/Match.java
+++ src/main/java/com/lol/highlight/domain/match/entity/Match.java
@@ -56,13 +56,16 @@ public class Match extends BaseEntity {
     private Integer item5;
     private Integer item6;
 
+    // 게임 버전 (패치 버전)
+    private String gameVersion;
+
     @Builder
     public Match(String puuid, String matchId, String championName, Integer kills,
                 Integer deaths, Integer assists, Double kda, Boolean win,
                 Integer gameDuration, Long gameCreation, MatchStatus status,
                 String timelineData, String detailDataUrl,
                 Integer item0, Integer item1, Integer item2, Integer item3,
-                Integer item4, Integer item5, Integer item6) {
+                Integer item4, Integer item5, Integer item6, String gameVersion) {
         this.puuid = puuid;
         this.matchId = matchId;
         this.championName = championName;
@@ -83,6 +86,7 @@ public class Match extends BaseEntity {
         this.item4 = item4;
         this.item5 = item5;
         this.item6 = item6;
+        this.gameVersion = gameVersion;
     }
 
     public void updateMatchData(String championName, Integer kills, Integer deaths,
